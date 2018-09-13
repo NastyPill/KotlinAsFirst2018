3e05e7af66ca655e8f11f91024e3f80c5a724456
NastyPill <DJPonnYash.Vinyl@gmail.com>
2018-09-13 20:32:05 +0300

    lesson1, mistakes was destroyed

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -25,7 +25,8 @@ fun sqr(x: Double) = x * x
  *
  * Вычисление дискриминанта квадратного уравнения
  */
-fun discriminant(a: Double, b: Double, c: Double) = sqr(b) - 4 * a * c
+fun discriminant(a: Double, b: Double, c: Double) =
+        sqr(b) - 4 * a * c
 
 /**
  * Пример
@@ -69,7 +70,8 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = hours * 3600 + minute
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = 0.04445 * (sagenes * 48  + arshins * 16  + vershoks)
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =
+        0.04445 * (sagenes * 48  + arshins * 16  + vershoks)
 
 
 /**
@@ -78,7 +80,8 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = 0.04445 
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double = (grad + min / 60 + sec / 3600) * PI /  180
+fun angleInRadian(grad: Int, min: Int, sec: Int): Double =
+        (grad + min / 60.0 + sec / 3600.0) * PI /  180.0
 
 /**
  * Тривиальная
@@ -86,7 +89,8 @@ fun angleInRadian(grad: Int, min: Int, sec: Int): Double = (grad + min / 60 + se
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double= sqrt(sqr(x2-x1)+sqr(y2-y1))
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double =
+        sqrt(sqr (x2 - x1) + sqr(y2 - y1))
 
 
 
@@ -105,7 +109,8 @@ fun thirdDigit(number: Int): Int = number / 100 % 10
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = hoursArrive * 60 + minutesArrive - hoursDepart * 60 - minutesDepart
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int =
+        hoursArrive * 60 + minutesArrive - hoursDepart * 60 - minutesDepart
 
 
 /**
@@ -132,5 +137,6 @@ fun accountInThreeYears(initial: Int, percent: Int): Double
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = number / 100 + number / 10 % 10 * 10 + number % 10 * 100
+fun numberRevert(number: Int): Int =
+        number / 100 + number / 10 % 10 * 10 + number % 10 * 100
 
