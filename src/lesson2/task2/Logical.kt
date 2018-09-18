b1456724fd198af2b31ebbec78cf74d41565fd37
NastyPill <DJPonnYash.Vinyl@gmail.com>
2018-09-18 22:29:12 +0300

    lesson2 task1 all + task2 test

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -2,6 +2,7 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import kotlin.math.abs
 
 /**
  * Пример
@@ -17,7 +18,13 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean
+{
+    return if (number / 1000 + number / 100 % 10 == number % 1000 + number % 100 / 10)
+        true
+    else
+        false
+}
 
 /**
  * Простая
@@ -26,7 +33,13 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean
+{
+    return if (x1 == x2 || y1 == y2 || abs(x1 - x2) == abs(y1 - y2))
+        true
+    else
+        false
+}
 
 
 /**
@@ -35,7 +48,18 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int
+{
+    if (year % 4 == 0 && month == 2)
+        return 29
+    else
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+            return 31
+        else
+            return 30
+
+
+}
 
 /**
  * Средняя
@@ -56,4 +80,12 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean
+{
+    return if (a <= r && b <= s || b <= r && a <= s || a <= r && c <= s ||
+                c <= r && a <= s || c <= r && b <= s || b <= r && c <= s)
+                true
+            else
+                false
+
+}
